13953c4dc06c941aec0b666f231bb623727974ee
Divyadinesh12 <149932116+Divyadinesh12@users.noreply.github.com>
2024-04-14 11:18:55 +0530

    Add files via upload

---
+++ Data/data1.py
@@ -0,0 +1,8 @@
+
+class WebData:
+    def __init__(self):
+        """
+            This class is used to contain all the data that are required to perform the testing for the add to cart button
+        """
+        self.url="https://tutorialsninja.com/demo/"
+        self.quantity="2"
---
+++ Data/data2.py
@@ -0,0 +1,16 @@
+class WebData:
+    """
+    This class is used to contain all the data that are required to perform the testing for the checkout page
+    """
+    def __init__(self):
+        self.url = "https://tutorialsninja.com/demo/"
+        self.first_Name="Divya"
+        self.last_name="Dinesh"
+        self.email="vignesh@gmail.com"
+        self.passwort="div12$!&"
+        self.confirm_password="div12$!&"
+        self.phone_no="2367899"
+        self.address="2/9 , Vedadsanthur, Dindigul"
+        self.city="Dinigul"
+        self.post_code="560 890"
+
